f93127bfcb9d9dfc5b1dffbe39b15885d355aa32
Valton <izzz.toni@gmail.com>
2023-10-31 18:34:19 +0100

    fixed mode

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react'
+import React, {useEffect} from 'react'
 import { useTheme } from 'next-themes'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons' 
@@ -8,7 +8,15 @@ import { IconProp } from '@fortawesome/fontawesome-svg-core';
 const ThemeSwitcher = () => {
     const { theme, setTheme } = useTheme()
     const faPropIcon = faMoon as IconProp;
-    const faPropIcon2 = faSun as IconProp;
+    
+    useEffect(() => {
+      if (theme === "system") {
+        const prefersDarkMode = window.matchMedia?.("(prefers-color-scheme: dark)").matches;
+        setTheme(prefersDarkMode ? "dark" : "light");
+      } else {
+        setTheme(theme === "dark" ? "dark" : "light");
+      }
+    }, [])
 
   return (
     <div>
